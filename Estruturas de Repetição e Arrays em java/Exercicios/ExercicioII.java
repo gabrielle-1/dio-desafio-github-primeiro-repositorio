170d6a7e749c989f3da91498b4677beb664e8603
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2022-07-10 13:40:47 -0300

    Exercício ler notas e impedir notas inválidas

---
+++ Estruturas de Repetição e Arrays em java/Exercicios/ExercicioII.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ExercicioII {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Digite a nota:");
+        nota = scan.nextInt();
+
+        while (nota < 0 || nota > 10) {
+            System.out.println("Nota inválida! Digite novamente:");
+            nota = scan.nextInt();
+        }
+
+    }
+}
